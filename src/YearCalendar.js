rwtaggart/time-off-planner

commit 3951d2742a69d9c268d7fbdee065ddab65e7a74d

fix: set year 2024

src/YearCalendar.js
@@ -27,6 +27,8 @@ import dateCalendarTheme from './DateCalendarTheme';
 
 dayjs.extend(dayOfYear)
 
+const YEAR=2024
+
 // TODO: Add to utils.js file ?
 export function condenseDates(dates) {
   return dates.reduce((abbrv, currDateSlot) => {
@@ -221,10 +223,10 @@ export function CalendarGrid({ days, dispatchDays }) {
               // monthsPerRow={3}
               key={"DateCalendar_" + monthId}
               sx={{fontSize: '0.5em'}}
-              value={null}
+              value={dayjs().year(YEAR)}
               views={['day']}
-              minDate={dayjs().month(monthId).date(1)}
-              maxDate={dayjs().month(monthId + 1).date(0)}
+              minDate={dayjs().year(YEAR).month(monthId).date(1)}
+              maxDate={dayjs().year(YEAR).month(monthId + 1).date(0)}
               // NOTE: day is undefined when onChange is clicked on a holiday for some reason...
               shouldDisableDate={day => !day || (day && (day.day() == 0 || day.day() == 6))}
               slots={{

commit 6b66e10737f94fbe64db543dd198623dfcb05813

fix: fix bug introduced in 3951d274

src/YearCalendar.js
@@ -27,7 +27,7 @@ import dateCalendarTheme from './DateCalendarTheme';
 
 dayjs.extend(dayOfYear)
 
-const YEAR=2024
+const YEAR=2025
 
 // TODO: Add to utils.js file ?
 export function condenseDates(dates) {
@@ -223,7 +223,7 @@ export function CalendarGrid({ days, dispatchDays }) {
               // monthsPerRow={3}
               key={"DateCalendar_" + monthId}
               sx={{fontSize: '0.5em'}}
-              value={dayjs().year(YEAR)}
+              value={null}
               views={['day']}
               minDate={dayjs().year(YEAR).month(monthId).date(1)}
               maxDate={dayjs().year(YEAR).month(monthId + 1).date(0)}